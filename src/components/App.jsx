6d05d486a2f28261fe1aa5f55355a41eff538296
Merano <merano@MeranodeMacBook-Pro.local>
2022-07-18 15:50:25 +0800

    # This is a combination of 3 commits. # This is the 1st commit message:
    solve autoprefixer error
    
    # This is the commit message #2:
    
    finish ProgressControl link with Steps part
    
    # This is the commit message #3:
    
    finish ProgressControl link with Steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,66 @@ import Cart from './Cart';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
-import ProgressControl from './ProgressControl';
+// import ProgressControl from './ProgressControl';
 import Footer from './Footer';
+import React from "react";
 
-// import 'css/main.scss';
+//for stepBtn
+import './ProgressControl/ProgressControl.scss'
+import { ReactComponent as LeftArrow } from '../assets/icons/left-arrow.svg';
+import { ReactComponent as ReftArrow } from '../assets/icons/right-arrow.svg';
 
-const App = () => {
+const STEP_MAP = [Step1, Step2, Step3];
+
+
+
+
+export default function App() {
+
+  const [stepIdx, setIdx] = React.useState(0);
+  let StepControl = STEP_MAP[stepIdx];
+  const StepPhase = () => {
+    setIdx(stepIdx + 1)
+  }
+
+  const TempBtn = () => {
+    return (
+      <section className="progress-control col col-12 ">
+        <div className="d-flex justify-content-between">
+          <button
+            className="btn pre-btn px-5 py-2"
+            type="button"
+            data-active={stepIdx > 0}
+            disabled={!(stepIdx > 0)}
+            onClick={() => setIdx(stepIdx - 1)} >
+            <LeftArrow className="left-arrow" alt="" />
+            上一步
+          </button>
+          <button
+            className="btn next-btn btn-primary px-5 py-2"
+            type="button"
+            disabled={!(stepIdx < 2)}
+            onClick={() => StepPhase()} >
+            {stepIdx < 2 ? '下一步' : '結帳'}
+            <ReftArrow className="right-arrow" alt="" />
+          </button>
+        </div>
+      </section>
+    );
+  };
+
+
+
   return (
     <>
       <Header />
       <main className="site-main">
         <div className="container main-container row">
           <section className="register-container col col-lg-7 col-sm-12">
-            <StepProgress />
-            <Step1 />
-            <Step2 />
-            <Step3 />
-            <ProgressControl />
+            <StepProgress StepPhase={stepIdx} />
+
+            <StepControl />
+            <TempBtn />
           </section>
           <section className="cart-container col col-lg-4 col-sm-12">
             <Cart />
@@ -32,4 +75,3 @@ const App = () => {
   );
 };
 
-export default App;
